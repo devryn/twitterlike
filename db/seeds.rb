0fce93a2381026691f3dee6138ab03edcf569430
Kate Walters <kawalters01@gmail.com>
2016-07-14 15:04:03 -0500

    Added 10 users with 1-10 tweets to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  User.create!(name: Faker::Name.name,
+  handle: Faker::Internet.username,
+  email: Faker::Internet.email)
+
+  3.times do
+    Tweet.create!(body: Faker::Hipster.sentence,
+    user_id: user.id)
+  end
+end
